1f66aed667f1bb297d5b9fb7337555d2829b6090
phamdinhquy@gmail.com <phamdinhquy@MacBook-Pro.local>
2025-03-19 14:57:42 -0700

    Handle subsequent alert's message following the first alert

--- tests/pom/uitestingplayground/AlertPage.ts
+++ tests/pom/uitestingplayground/AlertPage.ts
@@ -3,8 +3,12 @@ import { BasePage } from "../BasePage";
 
 export class AlertPage extends BasePage {
   private alertButton: Locator;
+  private confirmButton: Locator;
+  private promptButton: Locator;
 
   private message: string;
+  private defaultPromptMessage: string;
+  private subSequentMessage: string;
 
   constructor(page) {
     super(page);
@@ -12,6 +16,12 @@ export class AlertPage extends BasePage {
 
     this.alertButton = this.page.locator("#alertButton");
     // this.alertButton = this.page.getByRole("button", { name: "Alert" });
+
+    this.confirmButton = this.page.locator("#confirmButton");
+    // this.confirmButton = this.page.getByRole("button", { name: "Confirm" });
+
+    this.promptButton = this.page.locator("#promptButton");
+    // this.promptButton = this.page.getByRole("button", { name: "Prompt" });
   }
 
   async clickAlertButton(): Promise<void> {
@@ -26,12 +36,68 @@ export class AlertPage extends BasePage {
     return this.message;
   }
 
+  async getDefaultPromptMessage(): Promise<string> {
+    return this.defaultPromptMessage;
+  }
+
+  async getSubSequentMessage(): Promise<string> {
+    return this.subSequentMessage;
+  }
+
   /**
    * This method is no need because the alert is closed automatically
    */
   async closeAlert(): Promise<void> {
     this.page.on("dialog", async (dialog) => {
       await dialog.accept();
+    });
+  }
+
+  async clickConfirmButton(): Promise<void> {
+    this.page.on("dialog", async (dialog) => {
+      this.message = dialog.message();
+      await dialog.dismiss();
+    });
+    await this.confirmButton.click();
+  }
+
+  //   async clickPromptButton(): Promise<void> {
+  //     this.page.on("dialog", async (dialog) => {
+  //       console.log(
+  //         `Dialog type: ${dialog.type()}, message: ${dialog.message()}`
+  //       );
+
+  //       this.message = dialog.message();
+  //       this.defaultPromptMessage = dialog.defaultValue();
+
+  //       if (dialog.type() === "alert") this.subSequentMessage = dialog.message();
+
+  //       await dialog.accept("Hello, World!");
+  //     });
+  //     await this.promptButton.click();
+  //   }
+
+  async clickPromptButton(): Promise<void> {
+    return new Promise<void>(async (resolve) => {
+      this.page.on("dialog", async (dialog) => {
+        console.log(
+          `Dialog type: ${dialog.type()}, message: ${dialog.message()}`
+        );
+
+        if (dialog.type() === "prompt") {
+          this.message = dialog.message();
+          this.defaultPromptMessage = dialog.defaultValue();
+          await dialog.accept("Hello, World!");
+        } else if (dialog.type() === "alert") {
+          this.subSequentMessage = dialog.message();
+          await dialog.dismiss();
+          resolve(); // Resolve the promise after the second dialog
+        } else {
+          await dialog.dismiss();
+          resolve();
+        }
+      });
+      await this.promptButton.click();
     });
   }
 }
--- tests/uitestingplayground/alert.spec.ts
+++ tests/uitestingplayground/alert.spec.ts
@@ -4,8 +4,24 @@ import { test } from "../fixture";
 test("test alert", async ({ alertPage }) => {
   await alertPage.goToPage();
   await alertPage.clickAlertButton();
+});
 
+test("test alert Confirm", async ({ alertPage }) => {
+  await alertPage.goToPage();
+  await alertPage.clickConfirmButton();
+  expect
+    .soft(await alertPage.getAlertText())
+    .toBe("Today is Friday.\n" + "Do you agree?");
+});
+
+test("test alert Pronpt", async ({ alertPage }) => {
+  await alertPage.goToPage();
+  await alertPage.clickPromptButton();
   expect(await alertPage.getAlertText()).toBe(
-    "Today is a working day.\n" + "Or less likely a holiday."
+    "Choose \"cats\" or 'dogs'." + "\n" + "Enter your value:"
   );
+
+  expect(await alertPage.getDefaultPromptMessage()).toBe("cats");
+
+  expect(await alertPage.getSubSequentMessage()).toContain("Hello, World!");
 });
